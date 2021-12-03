bfc43a039f3fa601d1260ac3ccdbaeccf4e2a3a2
unknown <memichelle473@gmail.com>
2021-12-03 10:38:45 -0800

    api v1

---
+++ spring-boot-swagger2-ejemplo/src/main/java/com/example/springbootswagger2ejemplo/SpringBootSwagger2EjemploApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger2ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwagger2EjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2EjemploApplication.class, args);
+	}
+
+}
---
+++ spring-boot-swagger2-ejemplo/src/test/java/com/example/springbootswagger2ejemplo/SpringBootSwagger2EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger2ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger2EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
